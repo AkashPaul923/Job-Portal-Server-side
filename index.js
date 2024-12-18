430238092d1313332f297e8e00fc297c630da05a
AkashPaul923 <akashpaul923@gmail.com>
2024-12-18 23:36:29 +0600

    generate token and set in cookie

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 require('dotenv').config()
 const express = require('express');
 const cors = require('cors');
+const jwt = require('jsonwebtoken')
+const cookieParser = require('cookie-parser');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const req = require('express/lib/request');
 const app = express();
 const port = process.env.PORT ||5000;
 app.use(cors());
 app.use(express.json());
+app.use(cookieParser())
 
 
 
@@ -27,6 +31,16 @@ async function run() {
 
     const jobCollection = client.db('JobPortal').collection('Jobs')
     const jobApplicationCollection = client.db('JobPortal').collection('Job-Application')
+
+    // Auth APIs
+    app.post('/jwt', async (req, res)=>{
+      const user = req.body
+      const token = jwt.sign(user, process.env.JWT_SECRET, {expiresIn: '1h'})
+      res.cookie('token', token, {
+        httpOnly:true,
+        secure: false, 
+      }).send({success: true})
+    })
 
     // job APIs
     app.get('/jobs', async (req,res)=>{
